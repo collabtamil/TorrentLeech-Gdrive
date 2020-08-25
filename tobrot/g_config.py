dba25bc8005ee56555ca19d3f4bd322da9f30889
collabtamil <70214364+collabtamil@users.noreply.github.com>
2020-08-25 20:05:53 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,16 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1268fill---your----data"
-	APP_ID = 1373938
-	API_HASH = "fill--your--data"
-	OWNER_ID = "" #ID of bot owner
-	AUTH_CHANNEL = [-10082786282972]
-	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
-	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+	TG_BOT_TOKEN= "1221269735:AAHMbG34oCUml4sRplaXIl9eJS4Ylp8SaWM"
+	APP_ID = 1682719
+	API_HASH = "2edcaea55a3738985d979c3bf72b2e0d"
+	OWNER_ID = "1042281429" #ID of bot owner
+	AUTH_CHANNEL = [-1001227823555]
+	DESTINATION_FOLDER = "Kat rip" #Name of your folder read readme
+	RCLONE_CONFIG = """type = drive
+scope = drive
+root_folder_id = 0ABy1P7nxi3CPUk9PVA
+token = {"access_token":"ya29.a0AfH6SMBXflcs6ykhOgsA3x-DWS-6RpjYJvqobLAJGYfGC-bxwQ7TjiyjdP1c6coCDMwNDPsWCMw6J_jPNwJFduTZOqA7rPH5C3jUG9u2gpqHO9Uz5utwWXr4cMKHDdYj1IvdNnc_EeHuEd_Tl8W_wWCx97hhuCUhzOI","token_type":"Bearer","refresh_token":"1//0gBZITpEF5ZCJCgYIARAAGBASNwF-L9IrweLjQnzFPAQehBLOA82eQjsyK_Svo1IGfGAi-Yr1pdpEL27qaQxI5CpieXNToGkEBEQ","expiry":"2020-08-23T14:12:43.808833706Z"}
+team_drive = 0ABy1P7nxi3CPUk9PVA"""
+
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
